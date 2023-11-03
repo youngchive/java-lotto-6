e08cd3ccc2adaf184cf1f401ac857342c68cd1e5
youngchive <2000mily@naver.com>
2023-11-04 01:02:26 +0900

    feat: add lotto purchase calculation

--- src/main/java/lotto/BuyLotto.java
+++ src/main/java/lotto/BuyLotto.java
@@ -4,10 +4,18 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class BuyLotto {
     public static Integer buyLotto() {
+        Integer money = purchase();
+        int lottoTickets = money / 1000;
+
+        System.out.println(lottoTickets + "개를 구매했습니다.");
+        return lottoTickets;
+    }
+
+    public static Integer purchase() {
         System.out.println("구입금액을 입력해 주세요.");
         Integer input = Integer.valueOf(Console.readLine());
 
-        System.out.println(input);
+        System.out.println("구입금액: " + input);
         return input;
     }
 }
