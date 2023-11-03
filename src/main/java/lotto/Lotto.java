1a210f5067fc130d6bb972c1b4660300555f08ad
youngchive <2000mily@naver.com>
2023-11-04 00:48:54 +0900

    feat: generate lotto numbers

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -16,5 +19,28 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+
+    public static Lotto generateRandomLotto() {
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(randomNumbers); // 숫자를 오름차순으로 정렬
+        return new Lotto(randomNumbers);
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public static void main(String[] args) {
+        Lotto lotto = Lotto.generateRandomLotto();
+        List<Integer> numbers = lotto.getNumbers();
+
+        System.out.print("로또 번호: [");
+        for (int i = 0; i < numbers.size(); i++) {
+            System.out.print(numbers.get(i));
+            if (i < numbers.size() - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.print("]");
+    }
 }
